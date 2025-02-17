06e13f7b7be17f56e0d208dbc4a68d6a8db388d7
Maaa1k <m.a.lopez.bosch@gmail.com>
2025-02-17 17:13:32 +0100

    popular up

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+import "./popular.css";
+
 const Popular = () => {
-    return (
-      <div>
-        <h1>Popular Videos</h1>
-        <p>Aquí aparecerán los videos populares.</p>
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/popular")
+      .then((response) => response.json())
+      .then((data) => setVideos(data))
+      .catch((error) => console.error("Error fetching popular videos:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Popular Videos</h1>
+      <div className="popular-container">
+        {videos.map((video, index) => (
+          <div className="video-card" key={index}>
+            <img className="thumbnail" src={`http://localhost:3000${video.videoThumbnails[1].url}`}></img>
+            <div className="video-title">{video.title}</div>
+            <div className="video-author">{video.author}</div>
+            <div className="video-time">{video.publishedText}</div>
+          </div>
+        ))}
       </div>
-    );
-  };
-  
-  export default Popular;
-  
+    </div>
+  );
+};
+
+export default Popular;
